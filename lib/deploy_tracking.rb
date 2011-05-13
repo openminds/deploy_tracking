814c4c6c8b60c2cc96f29892ce771df190784cdd
Jeroen Jacobs <info@jeroenj.be>
2011-05-13 14:40:49 +0200

    Fixes an issue where branch could not be fetched

--- lib/deploy_tracking.rb
+++ lib/deploy_tracking.rb
@@ -4,22 +4,23 @@ configuration.load do
   namespace :deploy_tracking do
     desc "Add a new line to the Deploylog"
     task :log do
-      current_stage = fetch(:stage, 'production') 
-      deploy_file   = fetch(:deploy_file, "Deploylog")
+      current_stage = fetch :stage, 'production'
+      deploy_file = fetch :deploy_file, 'Deploylog'
+      branch = fetch :branch, 'master'
       git_info = `git log --oneline #{branch}|head -n 1`.chomp
-    
+
       File.open(deploy_file, 'a+') do |f|
         f.puts "[#{release_name}] Deployed to #{current_stage} by #{me.ljust(10)} - #{branch}:#{git_info}"
       end
-      
-      `git add #{deploy_file} && git commit -m "Updating deploylog for deploy #{release_name}" #{deploy_file}`
+
+      # `git add #{deploy_file} && git commit -m "Updating deploylog for deploy #{release_name}" #{deploy_file}`
     end
-    
+
     task :mark do
       put "Deployed by #{me}", "#{latest_release}/deployed_by"
     end
   end
-  
+
   before "deploy", "deploy_tracking:log"
   before "deploy:finalize_update", "deploy_tracking:mark"
 end
